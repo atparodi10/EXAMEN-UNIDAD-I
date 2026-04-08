c1d58538e6bc2af0bd4d534c85b6016bbb7046e3
meringe4444 <ghostform13.st@gmail.com>
2026-04-08 16:38:44 -0600

    captura de datos del cliente y creacion de cuenta bancaria

--- src/Caso2/Main.java
+++ src/Caso2/Main.java
@@ -1,4 +1,41 @@
 package Caso2;
 
+import Caso2.Cliente;
+import Caso2.Cuenta;
+
+import java.util.Scanner;
+
+// CESAR
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Sistema Bancario: ");
+
+        System.out.print("Ingresa la identificacion del cliente: ");
+        String identificacion = scanner.nextLine();
+
+        System.out.print("Ingresa los nombres del cliente: ");
+        String nombres = scanner.nextLine();
+
+        System.out.print("Ingresa los apellidos del cliente: ");
+        String apellidos = scanner.nextLine();
+
+        System.out.print("Ingresa el numero de cuenta: ");
+        String numeroCuenta = scanner.nextLine();
+
+        double saldoInicial;
+        do {
+            System.out.print("Ingrese el saldo inicial:  ");
+            saldoInicial = scanner.nextDouble();
+
+            if (saldoInicial < 0) {
+                System.out.println("Error: no es permitido saldos negativos.");
+            }
+        } while (saldoInicial < 0);
+
+        Cliente cliente = new Cliente(identificacion, nombres, apellidos);
+        Cuenta cuenta = new Cuenta(numeroCuenta, cliente, saldoInicial);
+    }
 }
