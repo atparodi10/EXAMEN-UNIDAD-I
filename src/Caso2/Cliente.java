2c2fda6cdacc5a39f8d71c55b85b65c4e75217ff
“cuchiflu29” <meaguilar@uamv.edu.ni>
2026-04-08 16:09:13 -0600

    clase cliente Marlon

--- src/Caso2/Cliente.java
+++ src/Caso2/Cliente.java
@@ -1,4 +1,19 @@
 package Caso2;
 
 public class Cliente {
+
+        private String nombre;
+
+        public Cliente(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+    }
 }
